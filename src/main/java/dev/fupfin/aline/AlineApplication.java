67a401f920384c53f7fc2805e8bdf4cbdbfea158
박성철 <fupfin@woowahan.com>
2021-06-03 18:08:36 +0900

    initial commit

---
+++ src/main/java/dev/fupfin/aline/AlineApplication.java
@@ -0,0 +1,13 @@
+package dev.fupfin.aline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/fupfin/aline/AlineApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fupfin.aline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
